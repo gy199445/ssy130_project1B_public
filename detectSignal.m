fb0ab0da2576daf6ff72b28ab665fc2712512969
Simon Nilsson <nisimon@student.chalmers.se>
2016-11-29 13:33:00 +0100

    Added better comments to energy detector

--- detectSignal.m
+++ detectSignal.m
@@ -1,10 +1,9 @@
 function [ sos, eos ] = detectSignal( signal, threshold, N_f )
 %DETECTSIGNAL Simple frame based energy detector
+%   Inputs: signal, SNR to detect signal, number of samples per frame
 %   Returns indices for start and end of signal
-
-
     
-    % Use first 10 samples to determine noise floor
+    % Use first 10 frames to determine noise floor
     noise_floor = sum(abs(signal(1:10*N_f)))/10;
 
     % If 0 noise, use perfect detection
@@ -14,6 +13,7 @@ function [ sos, eos ] = detectSignal( signal, threshold, N_f )
         return;
     end
     
+    % Determine energy threshold from noise floor
     eps = noise_floor*threshold;
     
     sos = 0; % return 0 if no signal found
@@ -22,13 +22,16 @@ function [ sos, eos ] = detectSignal( signal, threshold, N_f )
     L = length(signal);
     % Detect start of signal
     while i < L - N_f && sos == 0
+        % Detect start if average frame amplitude is above energy threshold
         if sum(abs(signal(i:(i + N_f - 1)))) > eps
             sos = i;
         end
+        % Advance one frame
         i = i + N_f;
     end
-    % Detect end of signal
+    % Detect end of signal after having found the start
     while i < L - N_f && eos == 0
+        % Detect end if average frame amplitude is below energy threshold
         if sum(abs(signal(i:(i + N_f - 1)))) < eps
             eos = i + N_f - 1;
         end
